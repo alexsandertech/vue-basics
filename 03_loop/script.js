cb73fb25610e270b47a527f7e0471dbf006e39dc
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2023-08-07 21:40:51 -0300

    feat: 03_loop

---
+++ 03_loop/script.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+    el: '#lista',
+    data: {
+        nomes: [
+            { nome: 'Nome 01', sobrenome: 'Sobrenome 01', idade: '01' },
+            { nome: 'Nome 02', sobrenome: 'Sobrenome 02', idade: '02' },
+            { nome: 'Nome 03', sobrenome: 'Sobrenome 03', idade: '03' },
+            { nome: 'Nome 04', sobrenome: 'Sobrenome 04', idade: '04' },
+        ]
+    }
+});
